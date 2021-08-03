1a82a55c0e08469db48baa3250e74a2ed9ebe4a9
webgl-widgets <webgl-widgets@program>
2021-08-03 20:55:12 +0300

    i revert "i use deps-repl library"
    This reverts commit e7080b837dfef22bcf06db4386fb499d79d56157.

--- program.sh
+++ program.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 repl(){
-  clj \
-    -X:repl deps-repl.core/process \
-    :main-ns webgl-widgets.main \
-    :port 7788 \
-    :host '"0.0.0.0"' \
-    :repl? true \
-    :nrepl? false
+  clj -A:repl
 }
 
 "$@"
